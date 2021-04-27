c58a454083fde8478311a051d6c7e30ab45059f5
JSukenik98 <72565574+JSukenik98@users.noreply.github.com>
2021-04-26 22:12:53 -0500

    Add files via upload

--- semantics.h
+++ semantics.h
@@ -1,7 +1,7 @@
 #include "SymTab.h"
 
 extern void printSymTab();
-extern void storeVar(char* name, SymTab table);
+extern void storeVar(char* name, SymTab* table);
 extern SymTab* doUnion(SymTab* table1, SymTab* table2);
 extern SymTab* doIntersect(SymTab* table1, SymTab* table2);
 extern SymTab* getVal(char* name);
