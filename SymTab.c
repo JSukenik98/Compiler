4afa359de7a8893e0973599f7269b5c5370b8fb4
JSukenik98 <72565574+JSukenik98@users.noreply.github.com>
2021-04-28 15:07:00 -0500

    Add files via upload

--- SymTab.c
+++ SymTab.c
@@ -211,7 +211,7 @@ int nextEntry(SymTab* table)
 {
 	if (hasCurrent(table))
 	{
-		fprintf(stderr, "Next Entry Current Pos: %d < %d", pos, table->size);
+		fprintf(stderr, "Next Entry Current Pos: %d < %d\n", pos, table->size);
 		while (pos < table->size)
 		{
 			if (table->current != NULL)
