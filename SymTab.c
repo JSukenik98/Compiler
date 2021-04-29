229186afca9ddb69bf2aca36c19d0ac343ebcacf
JSukenik98 <72565574+JSukenik98@users.noreply.github.com>
2021-04-29 00:53:33 -0500

    Add files via upload

--- SymTab.c
+++ SymTab.c
@@ -10,6 +10,7 @@ SymTab * createSymTab(int size)
 		SymTab* table = malloc(sizeof(SymTab));
 		table->contents = malloc(size * sizeof(SymEntry));
 		table->current = malloc(sizeof(SymEntry));
+		table->current = NULL;
 		table->size = size;
 		table->pos = 0;
 		return table;
